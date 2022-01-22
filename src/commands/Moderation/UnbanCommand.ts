1c41c228cb97a2a4ef6a54d94d996c7f50301b06
Dantavious Williams <66193634+n4n-0@users.noreply.github.com>
2022-01-21 22:59:53 -0800

    Added an unban command

---
+++ src/commands/Moderation/UnbanCommand.ts
@@ -0,0 +1,48 @@
+import { Message, MessageEmbed } from 'discord.js';
+import BaseCommand from '../../utils/structures/BaseCommand';
+import DiscordClient from '../../client/client';
+import { getRepository } from 'typeorm';
+import { BanLogConfiguration } from '../../../typeorm/entities/BanLogConfiguration';
+const colors = require('colors');
+const log = console.log;
+
+export default class UnbanCommand extends BaseCommand {
+  constructor(
+    private readonly banLogRespository = getRepository(BanLogConfiguration)
+  ) {
+    super('unban', 'Moderation', []);
+  }
+
+  async run(client: DiscordClient, message: Message, args: Array<string>) {
+    message.delete();
+
+    if (!message.member?.permissions.has('BAN_MEMBERS')) return;
+
+    const user = args[0]
+    if(!user) {
+      const cantFindUser = new MessageEmbed()
+        .setColor("#9d0101")
+        .setTitle('Please enter a valid user')
+        .setDescription(`Usage: ${client.prefix}unban [user]`)
+        .setFooter({ text: `${message.guild?.name} Moderation :: ${message.author.username}` })  
+      message.channel.send({ embeds: [cantFindUser] })
+      return
+    }
+
+    message.guild?.bans.fetch().then(banned => {
+      const bannedUser = banned.find(u => u.user.username === user);
+      if (!bannedUser) {
+        message.channel.send(`Cant find user ${user}`);
+      } else {
+        this.banLogRespository.delete({ guildId: message.guildId!, username: user });
+        message.guild?.members.unban(bannedUser.user.id);
+        const unBanEmbed = new MessageEmbed()
+          .setColor('#9d0101')
+          .setTitle('🗒 Unban')
+          .setDescription(`${bannedUser.user.username} has been unbanned from ${message.guild!.name} by ${message.author}`)
+          .setFooter({ text: `${message.guild?.name} Moderation :: ${message.author.username}` })
+        message.channel.send({ embeds: [unBanEmbed] })
+      }
+    })
+  }
+}
